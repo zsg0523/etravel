c248df99944689863bb28fa5d0e60db3fe8a9a5b
Eden <shenggen93@163.com>
2018-12-05 03:48:55 +0000

    add heroku database config

--- bootstrap/helpers.php
+++ bootstrap/helpers.php
@@ -4,5 +4,28 @@
  * @Author: Eden
  * @Date:   2018-12-03 10:31:17
  * @Last Modified by:   Eden
- * @Last Modified time: 2018-12-03 11:39:22
+ * @Last Modified time: 2018-12-05 11:57:49
  */
+
+function get_db_config()
+{
+    if (getenv('IS_IN_HEROKU')) {
+        $url = parse_url(getenv("DATABASE_URL"));
+
+        return $db_config = [
+            'connection' => 'pgsql',
+            'host' => $url["host"],
+            'database'  => substr($url["path"], 1),
+            'username'  => $url["user"],
+            'password'  => $url["pass"],
+        ];
+    } else {
+        return $db_config = [
+            'connection' => env('DB_CONNECTION', 'mysql'),
+            'host' => env('DB_HOST', 'localhost'),
+            'database'  => env('DB_DATABASE', 'forge'),
+            'username'  => env('DB_USERNAME', 'forge'),
+            'password'  => env('DB_PASSWORD', ''),
+        ];
+    }
+}
