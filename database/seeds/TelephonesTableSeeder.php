d61d2353a790adb4401423e285173c02b9962ce3
Eden <shenggen93@163.com>
2018-12-27 03:50:49 +0000

    add travel_id telephone

--- database/seeds/TelephonesTableSeeder.php
+++ database/seeds/TelephonesTableSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Models\Telephone;
+use App\Models\Travel;
 use Illuminate\Database\Seeder;
 
 class TelephonesTableSeeder extends Seeder
@@ -12,13 +13,24 @@ class TelephonesTableSeeder extends Seeder
      */
     public function run()
     {
-    	//获取faker 实例
+        // 获取 travel_ids
+        $travel_ids = Travel::all()->pluck('id')->toArray();
+
+    	// 获取faker 实例
         $faker = app(Faker\Generator::class);
 
         $telephones = factory(Telephone::class)
-        			->times(50)
-        			->make();
-        //将数据集合插入数据库
+        			->times(200)
+        			->make()
+                    ->each(function ($telephone, $index)
+                     use ($travel_ids, $faker) 
+                     {
+                        // 从 travel_id 中随机获取数组
+                        $telephone->travel_id = $faker->randomElement($travel_ids);
+                     });
+
+
+        // 将数据集合插入数据库
         Telephone::insert($telephones->toArray());
     }
 }
