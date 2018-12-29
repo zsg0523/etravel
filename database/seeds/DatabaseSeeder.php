3c80fdc21345c6e4feae146016542231328a21dd
Eden <shenggen93@163.com>
2018-12-29 09:52:32 +0000

    leader seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,21 +11,21 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(UsersTableSeeder::class);
-        $this->call(TravelsTableSeeder::class);
-        $this->call(TelephonesTableSeeder::class);
+        // $this->call(UsersTableSeeder::class);
+        // $this->call(TravelsTableSeeder::class);
+        // $this->call(TelephonesTableSeeder::class);
         $this->call(AssembliesTableSeeder::class);
-        $this->call(SchoolsTableSeeder::class);
-        $this->call(StudentsTableSeeder::class);
-        $this->call(NewsTableSeeder::class);
-        $this->call(HotelsTableSeeder::class);
-        $this->call(RulesTableSeeder::class);
-        $this->call(GroupsTableSeeder::class);
-        $this->call(RoutesTableSeeder::class);
-        $this->call(SceneriesTableSeeder::class);
-        $this->call(FoodsTableSeeder::class);
-        $this->call(StudiesTableSeeder::class);
-        $this->call(QuestionsTableSeeder::class);
+        // $this->call(SchoolsTableSeeder::class);
+        // $this->call(StudentsTableSeeder::class);
+        // $this->call(NewsTableSeeder::class);
+        // $this->call(HotelsTableSeeder::class);
+        // $this->call(RulesTableSeeder::class);
+        // $this->call(GroupsTableSeeder::class);
+        // $this->call(RoutesTableSeeder::class);
+        // $this->call(SceneriesTableSeeder::class);
+        // $this->call(FoodsTableSeeder::class);
+        // $this->call(StudiesTableSeeder::class);
+        // $this->call(QuestionsTableSeeder::class);
     }
 
 }
