10b19a2d13af45dad3a7f8f5fcdcfe1cae241337
Eden <shenggen93@163.com>
2019-01-11 11:04:15 +0000

    heroku

--- app/Models/Traits/LastActivedAtHelper.php
+++ app/Models/Traits/LastActivedAtHelper.php
@@ -4,7 +4,7 @@
  * @Author: Eden
  * @Date:   2019-01-11 12:25:04
  * @Last Modified by:   Eden
- * @Last Modified time: 2019-01-11 17:36:17
+ * @Last Modified time: 2019-01-11 19:02:56
  * 记录 - 通过中间件过滤用户请求，记录用户访问时间到 redis 按日期区分的哈希表
  * 同步 - 新建命令，计划任务每天运行一次此命令，将昨日哈希表的数据同步到数据库中，并删除
  * 读取 - 优先读取当日哈希表里 Redis 的数据，如果没有则使用数据库的数据
@@ -20,54 +20,54 @@ trait LastActivedAtHelper
     protected $hash_prefix = 'etravels_last_actived_at_';
     protected $field_prefix = 'user_';
 
-    public function recordLastActivedAt()
-    {
-        // 获取今天的日期
-        $date = Carbon::now()->toDateString();
+    // public function recordLastActivedAt()
+    // {
+    //     // 获取今天的日期
+    //     $date = Carbon::now()->toDateString();
 
-        // Redis 哈希表的命名，如：larabbs_last_actived_at_2017-10-21
-        $hash = $this->hash_prefix . $date;
+    //     // Redis 哈希表的命名，如：larabbs_last_actived_at_2017-10-21
+    //     $hash = $this->hash_prefix . $date;
 
-        // 字段名称，如：user_1
-        $field = $this->field_prefix . $this->id;
+    //     // 字段名称，如：user_1
+    //     $field = $this->field_prefix . $this->id;
 
-        // dd(Redis::hGetAll($hash));
+    //     // dd(Redis::hGetAll($hash));
 
-        // 当前时间，如：2017-10-21 08:35:15
-        $now = Carbon::now()->toDateTimeString();
+    //     // 当前时间，如：2017-10-21 08:35:15
+    //     $now = Carbon::now()->toDateTimeString();
 
-        // 数据写入 Redis ，字段已存在会被更新
-        Redis::hSet($hash, $field, $now);
-    }
+    //     // 数据写入 Redis ，字段已存在会被更新
+    //     Redis::hSet($hash, $field, $now);
+    // }
 
-    public function syncUserActivedAt()
-    {
-    	// 获取昨天的日期
-    	$yesterday_date = Carbon::yesterday()->toDateString();
+    // public function syncUserActivedAt()
+    // {
+    // 	// 获取昨天的日期
+    // 	$yesterday_date = Carbon::yesterday()->toDateString();
 
-    	// Redis 哈希表的命名
-    	$hash = $this->hash_prefix . $yesterday_date;
+    // 	// Redis 哈希表的命名
+    // 	$hash = $this->hash_prefix . $yesterday_date;
 
-    	// 从 Redis 表里获取数据
-    	$dates = Redis::hGetAll($hash);
+    // 	// 从 Redis 表里获取数据
+    // 	$dates = Redis::hGetAll($hash);
 
-    	// 遍历并同步到数据库中
-    	foreach ($dates as $user_id => $actived_at) {
-    		// 将 user_1 替换成 1
-    		$user_id = str_replace($this->field_prefix, '', $user_id);
+    // 	// 遍历并同步到数据库中
+    // 	foreach ($dates as $user_id => $actived_at) {
+    // 		// 将 user_1 替换成 1
+    // 		$user_id = str_replace($this->field_prefix, '', $user_id);
 
-    		// 用户存在时将数据写入数据库
-    		if ($user = $this->find($user_id)) {
-    			$user->last_actived_at = $actived_at;
-    			$user->save();
-    		}
-    	}
+    // 		// 用户存在时将数据写入数据库
+    // 		if ($user = $this->find($user_id)) {
+    // 			$user->last_actived_at = $actived_at;
+    // 			$user->save();
+    // 		}
+    // 	}
 
-    	// 同步后删除 Reids 数据
-    	Redis::del($hash);
-    }
+    // 	// 同步后删除 Reids 数据
+    // 	Redis::del($hash);
+    // }
 
-
+    /** [getLastActivedAtAttribute 访问器 ] */
     // public function getLastActivedAtAttribute($value)
     // {
     // 	// 获取今天的日期
@@ -90,7 +90,7 @@ trait LastActivedAtHelper
     // 	}
  
 
-    // }
+    }
 
 
 
