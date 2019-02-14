758485d0f91f55c8f40ce0a464d70262b927c4cb
Eden <shenggen93@163.com>
2019-02-14 04:57:41 +0000

    example.config.js

--- resources/assets/js/example.config.js
+++ resources/assets/js/example.config.js
@@ -1,6 +1,5 @@
 
 
-// const baseUrl='http://www.bunchparty.com/etravel/public';//线上地址
-const baseUrl='http://etravel.test';
-// const baseUrl='';
+const baseUrl='http://www.bunchparty.com/etravel/public';//线上地址
+// const baseUrl='http://etravel.test';
 export default baseUrl
