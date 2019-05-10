c7007e1bd06f61cbc43db5a66a1ca70a4fda7ed3
Eden <shenggen93@163.com>
2019-05-10 08:04:22 +0000

    获取邮件验证码优化

---
+++ app/Mail/SendCode.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class SendCode extends Mailable implements ShouldQueue
+{
+    use Queueable, SerializesModels;
+
+    protected $code;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct($code)
+    {
+        $this->code = $code;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->markdown('emails.user.sendCode')->with(['code' => $this->code]);
+    }
+}
